b826c90a72ed19b6038b0c087716321e55d2443b
Andrew <andrewscott13@mittymonarch.com>
2015-04-08 14:39:01 -0700

    uncommented rest of tests

--- src/avltree-test.cpp
+++ src/avltree-test.cpp
@@ -1,67 +1,62 @@
 /**
- * \file splaytree-test.cpp
+ * \file avltree-test.cpp
  * \author Andrew Scott
  *
- * \brief Tests a SplayTree for correctness using multiple types
+ * \brief Tests an AvlTree for correctness using multiple types
  *
  * \details
- *   Configured to use the templated SplayTree found in splaytree.hpp as
- *   splay trees of different types
+ *   Configured to use the templated AvlTree found in avltree.hpp as
+ *   avl trees of different types
  *
  */
 
-#include "splaytree.hpp"
+#include "avltree.hpp"
 #include <iostream>
 #include <stdlib.h>      // rand(), srand()
 #include <time.h>       // time
 #include <gtest/gtest.h>
 #include "otter.hpp"
 
-// This test just checks that insert and contains() return the proper values.
-// To check that the splaying inserts and lookups were working, the trees were
-// printed out
-TEST(splayTreeIntTest, insertTests)
+TEST(avlTreeIntTest, insertTests)
 {
-    SplayTree<int> intTree;
-    int test = 220;
+    AvlTree<int> intTree;
+    int test = 0;
     EXPECT_FALSE(intTree.contains(test));
     bool inserted = intTree.insert(test);
     EXPECT_TRUE(intTree.contains(test));
     EXPECT_TRUE(intTree.size() == 1);
     EXPECT_TRUE(inserted);
-    int test2 = 210;
+    int test2 = 1;
     inserted = intTree.insert(test2);
     EXPECT_TRUE(intTree.contains(test2));
     EXPECT_TRUE(intTree.size() == 2);
     EXPECT_TRUE(inserted);
     // check that inserting again returns false
     EXPECT_FALSE(intTree.insert(test));
-    for (int i = 0; i < 20; ++i) {
+    for (int i = 2; i < 63; ++i) {
         EXPECT_FALSE(intTree.contains(i));
         intTree.insert(i);
         EXPECT_TRUE(intTree.contains(i));
+        EXPECT_TRUE(intTree.isBalanced());
     }
-    // these couple lookups will force the tree to splay something to the
-    // root that is not there yet, so they are helping to test the
-    // splayToRoot() function in the splay tree
-    EXPECT_TRUE(intTree.contains(5));
-    EXPECT_TRUE(intTree.contains(10));
-    EXPECT_TRUE(intTree.contains(15));
-    EXPECT_TRUE(intTree.contains(17));
-    EXPECT_TRUE(intTree.contains(7));
-    EXPECT_TRUE(intTree.contains(2));
-    EXPECT_TRUE(intTree.contains(220));
-    // checks that the contains() lookups reduced the height of the tree
-    EXPECT_TRUE(intTree.height() < 10);
+    EXPECT_TRUE(intTree.height() < 8);
     intTree.print(std::cout);
-    EXPECT_TRUE(intTree.contains(11));
-    intTree.print(std::cout);
+
+    AvlTree<int> intTree2;
+    for (int i = 0; i < 50; ++i) {
+        int intToInsert = rand() % 100;
+        //EXPECT_FALSE(intTree2.contains(intToInsert));
+        intTree2.insert(intToInsert);
+        EXPECT_TRUE(intTree2.contains(intToInsert));
+        EXPECT_TRUE(intTree.isBalanced());
+    }
+    intTree2.print(std::cout);
 }
 
-TEST(splayTreeIntTest, basicEqualityTests)
+TEST(avlTreeIntTest, basicEqualityTests)
 {
-    SplayTree<int> intTree;
-    SplayTree<int> intTree2;
+    AvlTree<int> intTree;
+    AvlTree<int> intTree2;
     // check that empty trees are equal
     EXPECT_TRUE(intTree == intTree2);
     int test = 120;
@@ -82,12 +77,12 @@ TEST(splayTreeIntTest, basicEqualityTests)
     ASSERT_NE(intTree, intTree2);
 }
 
-TEST(splayTreeIntTest, copyConstructorTests)
+TEST(avlTreeIntTest, copyConstructorTests)
 {
-    SplayTree<int> intTree;
+    AvlTree<int> intTree;
     int test = 120;
     intTree.insert(test);
-    SplayTree<int> intTree2{intTree};
+    AvlTree<int> intTree2{intTree};
     // tests copy constructor copying one element tree
     ASSERT_EQ(intTree, intTree2);
     int test2 = 220;
@@ -97,19 +92,19 @@ TEST(splayTreeIntTest, copyConstructorTests)
     for (int i = 0; i < 100; ++i) {
         intTree2.insert(i);
     }
-    SplayTree<int> intTree3{intTree2};
+    AvlTree<int> intTree3{intTree2};
     // test copying a larger tree
     // also tests equality operator on larger trees
     ASSERT_EQ(intTree2, intTree3);
     ASSERT_NE(intTree, intTree3);
 }
 
-TEST(splayTreeIntTest, assignmentOperatorTests)
+TEST(avlTreeIntTest, assignmentOperatorTests)
 {
-    SplayTree<int> intTree;
+    AvlTree<int> intTree;
     int test = 1234;
     intTree.insert(test);
-    SplayTree<int> intTree2;
+    AvlTree<int> intTree2;
     ASSERT_NE(intTree, intTree2);
     intTree2 = intTree;
     ASSERT_EQ(intTree, intTree2);
@@ -118,7 +113,7 @@ TEST(splayTreeIntTest, assignmentOperatorTests)
     }
     
     ASSERT_NE(intTree, intTree2);
-    SplayTree<int> intTree3;
+    AvlTree<int> intTree3;
     ASSERT_NE(intTree2, intTree3);
     intTree3 = intTree2;
     ASSERT_EQ(intTree2, intTree3);
@@ -126,21 +121,21 @@ TEST(splayTreeIntTest, assignmentOperatorTests)
     ASSERT_NE(intTree2, intTree3);
 }
 
-TEST(splayTreeIntTest, iteratorTests)
+TEST(avlTreeIntTest, iteratorTests)
 {
-    SplayTree<int> intTree;
+    AvlTree<int> intTree;
     for (int i = 0; i < 100; ++i) {
         intTree.insert(i);
     }
     int num = 0;
-    for (SplayTree<int>::iterator i = intTree.begin(); i != intTree.end(); ++i) {
+    for (AvlTree<int>::iterator i = intTree.begin(); i != intTree.end(); ++i) {
         ASSERT_EQ(num, *i);
         ++num;
     }
 }
 
-TEST(splayTreeIntTest, deleteElementTests) {
-    SplayTree<int> intTree;
+TEST(avlTreeIntTest, deleteElementTests) {
+    AvlTree<int> intTree;
     intTree.insert(5);
     // just an assurance that the delete is actually changing the value of
     // contains(5)
@@ -168,22 +163,23 @@ TEST(splayTreeIntTest, deleteElementTests) {
         ASSERT_EQ(intTree.size(), 4 - i);
     }
 
-    for (int i = 0; i < 200; ++i) {
+    for (int i = 0; i < 30; ++i) {
         intTree.insert(i);
     }
-    for (int i = 199; i >=0; --i) {
+    for (int i = 29; i >=0; --i) {
         // check that lots of deletes work
         EXPECT_TRUE(intTree.contains(i)) << "list should contain " << i;
         bool deleted = intTree.deleteElement(i);
         EXPECT_FALSE(intTree.contains(i));
         EXPECT_TRUE(deleted);
         ASSERT_EQ(intTree.size(), i);
+        EXPECT_TRUE(intTree.isBalanced());
     }
 }
-
-TEST(splayTreeOtterTest, insertTests)
+/*
+TEST(avlTreeOtterTest, insertTests)
 {
-    SplayTree<Otter> otterTree;
+    AvlTree<Otter> otterTree;
     Otter phokey = Otter{"phokey"};
     EXPECT_FALSE(otterTree.contains(phokey));
     bool inserted = otterTree.insert(phokey);
@@ -206,10 +202,10 @@ TEST(splayTreeOtterTest, insertTests)
     }
 }
 
-TEST(splayTreeOtterTest, basicEqualityTests)
+TEST(avlTreeOtterTest, basicEqualityTests)
 {
-    SplayTree<Otter> otterTree;
-    SplayTree<Otter> otterTree2;
+    AvlTree<Otter> otterTree;
+    AvlTree<Otter> otterTree2;
     // check that empty trees are equal
     EXPECT_TRUE(otterTree == otterTree2);
     Otter phokey = Otter{"phokey"};
@@ -231,12 +227,12 @@ TEST(splayTreeOtterTest, basicEqualityTests)
     ASSERT_NE(otterTree, otterTree2);
 }
 
-TEST(splayTreeOtterTest, copyConstructorTests)
+TEST(avlTreeOtterTest, copyConstructorTests)
 {
-    SplayTree<Otter> otterTree;
+    AvlTree<Otter> otterTree;
     Otter phokey = Otter{"phokey"};
     otterTree.insert(phokey);
-    SplayTree<Otter> otterTree2{otterTree};
+    AvlTree<Otter> otterTree2{otterTree};
     // tests copy constructor copying one element tree
     ASSERT_EQ(otterTree, otterTree2);
     Otter test2 = Otter{"another"};
@@ -247,19 +243,19 @@ TEST(splayTreeOtterTest, copyConstructorTests)
         Otter o{std::to_string(i)};
         otterTree2.insert(o);
     }
-    SplayTree<Otter> otterTree3{otterTree2};
+    AvlTree<Otter> otterTree3{otterTree2};
     // test copying a larger tree
     // also tests equality operator on larger trees
     ASSERT_EQ(otterTree2, otterTree3);
     ASSERT_NE(otterTree, otterTree3);
 }
 
-TEST(splayTreeOtterTest, assignmentOperatorTests)
+TEST(avlTreeOtterTest, assignmentOperatorTests)
 {
-    SplayTree<Otter> otterTree;
+    AvlTree<Otter> otterTree;
     Otter phokey = Otter{"phokey"};
     otterTree.insert(phokey);
-    SplayTree<Otter> otterTree2;
+    AvlTree<Otter> otterTree2;
     ASSERT_NE(otterTree, otterTree2);
     otterTree2 = otterTree;
     ASSERT_EQ(otterTree, otterTree2);
@@ -269,7 +265,7 @@ TEST(splayTreeOtterTest, assignmentOperatorTests)
     }
     
     ASSERT_NE(otterTree, otterTree2);
-    SplayTree<Otter> otterTree3;
+    AvlTree<Otter> otterTree3;
     ASSERT_NE(otterTree2, otterTree3);
     otterTree3 = otterTree2;
     ASSERT_EQ(otterTree2, otterTree3);
@@ -277,21 +273,21 @@ TEST(splayTreeOtterTest, assignmentOperatorTests)
     ASSERT_NE(otterTree2, otterTree3);
 }
 
-TEST(splayTreeOtterTest, iteratorTests)
+TEST(avlTreeOtterTest, iteratorTests)
 {
-    SplayTree<Otter> otterTree;
+    AvlTree<Otter> otterTree;
     for (int i = 0; i < 100; ++i) {
         otterTree.insert(Otter{std::to_string(i)});
     }
     int num = 0;
-    for (SplayTree<Otter>::iterator i = otterTree.begin(); i != otterTree.end(); ++i) {
+    for (AvlTree<Otter>::iterator i = otterTree.begin(); i != otterTree.end(); ++i) {
         *i;
         ++num;
     }
 }
 
-TEST(splayTreeOtterTest, deleteElementTests) {
-    SplayTree<Otter> otterTree;
+TEST(avlTreeOtterTest, deleteElementTests) {
+    AvlTree<Otter> otterTree;
     Otter phokey = Otter{"phokey"};
     otterTree.insert(phokey);
     // just an assurance that the delete is actually changing the value of
@@ -316,4 +312,4 @@ TEST(splayTreeOtterTest, deleteElementTests) {
         EXPECT_TRUE(deleted);
         ASSERT_EQ(otterTree.size(), i);
     }
-}
+}*/
